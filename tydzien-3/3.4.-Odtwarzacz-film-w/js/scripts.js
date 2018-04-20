da40d3506cad2a14d0cd13d67bf24261cf455af9
pwkrz <pg.krzeminski@gmail.com>
2018-04-20 18:24:48 +0200

    Translated code to English, added cross-browser full screen handling

--- tydzien-3/3.4.-Odtwarzacz-film-w/js/scripts.js
+++ tydzien-3/3.4.-Odtwarzacz-film-w/js/scripts.js
@@ -4,120 +4,102 @@ window.onload = function(){
 	
 	var filmPlayer = {
 				
-		kontrolkiToggler: function(){
+		controlsToggler: function(){
 			
-			this.videoKontener.onmouseover = function(){
-				this.kontrolki.style.opacity = 1;
-				this.videoKontener.dataset.hovered = "true";
-				clearTimeout(this.kontrolki.controlsOut)
+			this.videoWrapper.onmouseover = function(){
+				this.controls.style.opacity = 1;
+				this.videoWrapper.dataset.hovered = "true";
+				clearTimeout(this.controls.controlsOut)
 			}.bind(this);
 			
-			this.videoKontener.onmouseout = function(){
-				this.videoKontener.dataset.hovered = "false";
-				this.kontrolki.controlsOut = setTimeout(function(){ this.kontrolki.style.opacity = ""; clearTimeout(this.kontrolki.controlsOut)}, 1000);
+			this.videoWrapper.onmouseout = function(){
+				this.videoWrapper.dataset.hovered = "false";
+				this.controls.controlsOut = setTimeout(function(){ this.controls.style.opacity = ""; clearTimeout(this.controls.controlsOut)}, 1000);
 			}.bind(this);
 			
-			(this.kontrolki.mute || this.kontrolki.volumeSlider).onmouseover = function(){
-				this.kontrolki.volumeSlider.style.display = "block";
+			(this.controls.mute || this.controls.volumeSlider).onmouseover = function(){
+				this.controls.volumeSlider.style.display = "block";
 			}.bind(this);
 			
-			(this.kontrolki.mute || this.kontrolki.volumeSlider).onmouseout = function(){
-				this.kontrolki.volumeSlider.style.display = "none";
+			(this.controls.mute || this.controls.volumeSlider).onmouseout = function(){
+				this.controls.volumeSlider.style.display = "none";
 			}.bind(this);
 			
 		},
 		
-		przypiszZdarzenia: {
+		assignEvents: {
 		
 			init: function(that){
 				
-				that.kontrolki.eject.onclick = that.otwieranieNowychFilmow.bind(that);
-				that.video.onwheel = that.zmianaGlosnosci.bind(that);
-				that.kontrolki.onwheel = that.zmianaGlosnosci.bind(that);
-				that.kontrolki.volumeSlider.addEventListener("click", function(e){ e.stopPropagation() }, false);
-				that.kontrolki.mute.addEventListener("click", that.muteUnmute.bind(that), false);
-				that.kontrolki.volumeRange.onchange = that.zmianaGlosnosci.bind(that);
-				// that.kontrolki.addEventListener("click", function(e){ e.stopPropagation() }, false);
-				that.kontrolki.fsButton.onclick = that.toggleFullScreen.bind(that);
+				that.controls.eject.onclick = that.ejectHandler.bind(that);
+				that.video.onwheel = that.volumeChange.bind(that);
+				that.controls.onwheel = that.volumeChange.bind(that);
+				that.controls.volumeSlider.addEventListener("click", function(e){ e.stopPropagation() }, false);
+				that.controls.mute.addEventListener("click", that.muteUnmute.bind(that), false);
+				that.controls.volumeRange.onchange = that.volumeChange.bind(that);
+				// that.controls.addEventListener("click", function(e){ e.stopPropagation() }, false);
+				that.controls.fsButton.onclick = that.toggleFullScreen.bind(that);
 				that.video.ondblclick = that.toggleFullScreen.bind(that);
 				
 			},
 			
-			reszta: function(that){
+			videoLoaded: function(that){
 				
-				that.kontrolki.playpause.onclick = that.togglePlay.bind(that);
+				that.controls.playpause.onclick = that.togglePlay.bind(that);
 				that.video.onclick = that.togglePlay.bind(that);
-				that.videoKontener.addEventListener("keypress", function(e){ console.log(e); if(e.keyCode === 32){ that.togglePlay() } }.bind(that), false);
-				that.kontrolki.stop.addEventListener("click", that.stopFilm.bind(that), false);
-				that.video.addEventListener("timeupdate", that.sledzeniePostepu.bind(that), false);
-				that.kontrolki.videoProgressRange.onchange = that.scrollPlayback.bind(that);
-				that.video.onended = that.koniecFilmu.bind(that)
+				that.videoWrapper.addEventListener("keypress", function(e){ console.log(e); if(e.keyCode === 32){ that.togglePlay() } }.bind(that), false);
+				that.controls.stop.addEventListener("click", that.stopFilm.bind(that), false);
+				that.video.addEventListener("timeupdate", that.trackProgress.bind(that), false);
+				that.controls.videoProgressRange.onchange = that.scrollPlayback.bind(that);
+				that.video.onended = that.videoEnded.bind(that)
 				
 			}
 		},
 		
-		koniecFilmu: function(){
+		videoEnded: function(){
 			
 			this.video.currentTime = 0;
 			this.video.paused = true;
 			this.faToggle("playpause", ["fa-play"], false)
 			
 		},
+
+		handleFullScreenChange: function(){
+			this.faToggle("fsButton", ["icon-enlarge", "icon-shrink"], false);
+
+			this.videoWrapper.classList.toggle("full-screen");
+		},
 		
 		toggleFullScreen: function(){
-			// (NIEUDANE) PODEJŚCIE DO CROSS-BROWSER FULL SCREEN
-			
-			// var _onFSchange = document.onfullscreenchange || document.onwebkitfullscreenchange || document.onmozfullscreenchange || document.MSFullscreenChange,
-				// _isFS = document.fullscreen || document.webkitIsFullScreen || document.mozFullScreen || window.screenTop == 0,
-				// _FSon = this.videoKontener.requestFullscreen || this.videoKontener.webkitRequestFullscreen || this.videoKontener.mozRequestFullScreen || this.videoKontener.msRequestFullscreen,
-				// _FSoff = document.exitFullscreen || document.webkitExitFullscreen || document.mozCancelFullScreen || document.msExitFullscreen;
-			
-			// _onFSchange = function(){ this.faToggle("fsButton", ["fa-arrows-alt", "fa-compress"], false); }.bind(this);
-			
-			// (!_isFS && _FSon()); (_isFS && _FSoff()); !! DLA FF DZIAŁA _FSon PRZY _FSon = this.videoKontener.webkitRequestFullscreen();
-			
-			// ****************************************
-			
-			if(document.mozFullScreen != undefined){
-			
-			document.onmozfullscreenchange = function(){ this.faToggle("fsButton", ["icon-enlarge", "icon-shrink"], false); }.bind(this);
-			
-			(!document.mozFullScreen && this.videoKontener.mozRequestFullScreen());
-			(document.mozFullScreen && document.mozCancelFullScreen());
-			
-			} else if(document.webkitFullscreenEnabled){
-			
-			document.onwebkitfullscreenchange = function(){ this.faToggle("fsButton", ["icon-enlarge", "icon-shrink"], false); }.bind(this);
-			
-			(!document.webkitIsFullScreen && this.videoKontener.webkitRequestFullscreen());
-			(document.webkitIsFullScreen && document.webkitExitFullscreen());
-			
-			} else { alert("Na razie nie działa!") }
-			
+			// CROSS-BROWSER FULL SCREEN HANDLING ATTEMPT
+
+			var _isFS = document.fullscreen || document.webkitIsFullScreen || document.mozFullScreen || window.innerHeight === screen.height;
+
+			if(_isFS){ document[this._exitFS](); } else { this.videoWrapper[this._requestFS](); };
 		},
 		
 		scrollPlayback: function(){
 			
-			this.video.currentTime = this.kontrolki.videoProgressRange.value / 100 * this.video.duration;
+			this.video.currentTime = this.controls.videoProgressRange.value / 100 * this.video.duration;
 			
 		},
 		
-		sledzeniePostepu: function(e){
+		trackProgress: function(e){
 			
-			this.kontrolki.videoProgressRange.value = ( this.video.currentTime / this.video.duration * 100 || "0" );
+			this.controls.videoProgressRange.value = ( this.video.currentTime / this.video.duration * 100 || "0" );
 			
 			var _dateObj = new Date(this.video.currentTime * 1000),
 				_czas = ( _dateObj.toUTCString().split(" ")[4] || "00:00:00" );
 			
-			this.kontrolki.timeProgress.innerHTML = _czas;
+			this.controls.timeProgress.innerHTML = _czas;
 		},
 		
 		stopFilm: function(){
 			this.video.src = "";
-			this.ustawDlugosc();
-			this.sledzeniePostepu();
+			this.setDuration();
+			this.trackProgress();
 			this.faToggle("playpause", ["icon-play3"], false)
-			this.zablokujKontrolki();
+			this.blockControls();
 		},
 		
 		togglePlay: function(){
@@ -127,21 +109,21 @@ window.onload = function(){
 			
 		},
 		
-		zmianaGlosnosci: function(e){
+		volumeChange: function(e){
 
-			if(e){ e.preventDefault(); this.kontrolki.volumeSlider.style.display = "block" }
+			if(e){ e.preventDefault(); this.controls.volumeSlider.style.display = "block" }
 			
 			if(e.deltaY < 0){
 				clearTimeout(_wheelTOut);
-				this.kontrolki.volumeRange.value = parseInt(this.kontrolki.volumeRange.value) + 5;
-				var _wheelTOut = setTimeout(function(){ this.kontrolki.volumeSlider.style.display = ""; clearTimeout(_wheelTOut) }, 2500)
+				this.controls.volumeRange.value = parseInt(this.controls.volumeRange.value) + 5;
+				var _wheelTOut = setTimeout(function(){ this.controls.volumeSlider.style.display = ""; clearTimeout(_wheelTOut) }, 2500)
 			} else if(e.deltaY > 0){
 				clearTimeout(_wheelTOut);
-				this.kontrolki.volumeRange.value -= 5;
-				var _wheelTOut = setTimeout(function(){ this.kontrolki.volumeSlider.style.display = ""; clearTimeout(_wheelTOut) }, 2500)
+				this.controls.volumeRange.value -= 5;
+				var _wheelTOut = setTimeout(function(){ this.controls.volumeSlider.style.display = ""; clearTimeout(_wheelTOut) }, 2500)
 			}
 			
-			this.video.volume = this.kontrolki.volumeRange.value / 100 ;
+			this.video.volume = this.controls.volumeRange.value / 100 ;
 			
 			if(this.video.muted){
 				this.faToggle("mute", ["icon-volume-medium", "icon-volume-mute2"], true);
@@ -158,102 +140,127 @@ window.onload = function(){
 			
 			this.faToggle("mute", ["icon-volume-medium", "icon-volume-mute2"], true);
 			
-			this.kontrolki.volumeRange.value = this.video.muted ? "0" : this._volPreMute;
+			this.controls.volumeRange.value = this.video.muted ? "0" : this._volPreMute;
 		},
 		
 		faToggle: function(el, onOff, bool){
-			(bool && this.kontrolki[el].classList.toggle("pressed"))
+			(bool && this.controls[el].classList.toggle("pressed"))
 			
-			var i = this.kontrolki[el].querySelector("i")
+			var i = this.controls[el].querySelector("i")
 			
-			onOff.length === 1 ? i.classList = "ic " + onOff[0] :
-			
-			i.outerHTML = (i.outerHTML.includes(onOff[0])? i.outerHTML.replace(onOff[0], onOff[1]) : i.outerHTML.replace(onOff[1], onOff[0]));
+			onOff.length === 1 ? i.classList = "ic " + onOff[0]
+							   : i.outerHTML = (i.outerHTML.includes(onOff[0]) ? i.outerHTML.replace(onOff[0], onOff[1])
+							   												   : i.outerHTML.replace(onOff[1], onOff[0]));
 		},
 		
-		odblokujKontrolki: function(){
+		unblockControls: function(){
 			
-			for(el in this.kontrolki){
-				try { if(this.kontrolki.hasOwnProperty(el) && this.kontrolki[el].dataset.disable == "true"){
-					this.kontrolki[el].classList.remove("disabled");
-				};} catch(e) { console.log("\"" + el + "\" nie ma atrybutu \"classList\" ponieważ jego typ to: " + (typeof this.kontrolki[el])) }
+			for(el in this.controls){
+				try { if(this.controls.hasOwnProperty(el) && this.controls[el].dataset.disable == "true"){
+					this.controls[el].classList.remove("disabled");
+				};} catch(e) { console.log("\"" + el + "\" does not contain \"classList\" property because its type is: " + (typeof this.controls[el])) }
 			};
 			
 			var _FSenabled = document.fullscreenEnabled || document.webkitFullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled;
-			if (!_FSenabled) this.kontrolki.fsButton.classList.add("disabled")
+			if (!_FSenabled) this.controls.fsButton.classList.add("disabled")
 			
-			this.przypiszZdarzenia.reszta(this);
+			this.assignEvents.videoLoaded(this);
 			
 		},
 		
-		zablokujKontrolki: function(){
+		blockControls: function(){
 			
-			for(el in this.kontrolki){
-				try { if(this.kontrolki.hasOwnProperty(el) && this.kontrolki[el].dataset.disable == "true"){
-					this.kontrolki[el].classList.add("disabled");
-				};} catch(e) { console.log("\"" + el + "\" nie ma atrybutu \"classList\" ponieważ jego typ to: " + (typeof this.kontrolki[el])) }
+			for(el in this.controls){
+				try { if(this.controls.hasOwnProperty(el) && this.controls[el].dataset.disable == "true"){
+					this.controls[el].classList.add("disabled");
+				};} catch(e) { console.log("\"" + el + "\" does not contain \"classList\" property because its type is: " + (typeof this.controls[el])) }
 			};			
 		},
 		
-		ustawDlugosc: function(){
+		setDuration: function(){
 			var _dateObj = new Date(this.video.duration * 1000),
 				_czas = ( _dateObj.toUTCString().split(" ")[4] || "00:00:00" );
 			
-			this.kontrolki.timeDuration.innerHTML = _czas;
+			this.controls.timeDuration.innerHTML = _czas;
 		},
 		
-		otwieranieNowychFilmow: function(){			
-			this.kontrolki.videoInput.click();
+		ejectHandler: function(){			
+			this.controls.videoInput.click();
 			
 			this.video.onloadeddata = function(){
-				this.ustawDlugosc();
-				this.kontrolki.stop.classList.contains("disabled") ? this.odblokujKontrolki() : this.faToggle("playpause", ["icon-play3"], false);
-				this.videoKontener.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+				this.setDuration();
+				this.controls.stop.classList.contains("disabled") ? this.unblockControls() : this.faToggle("playpause", ["icon-play3"], false);
+				this.videoWrapper.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 				window.URL.revokeObjectURL(this.url)
 			}.bind(this)
 			
-			this.kontrolki.videoInput.onchange = function(e){
-				if(this.kontrolki.videoInput.files[0].type != ("video/mp4" || "video/webm" || "video/ogg")){ alert("Odtwarzacz obsługuje tylko pliki w formatach MP4, OGG i WEBM."); return }
+			this.controls.videoInput.onchange = function(e){
+				if(this.controls.videoInput.files[0].type != ("video/mp4" || "video/webm" || "video/ogg")){ alert("This player supports only MP4, OGG and WEBM files."); return }
 				
-				this.url = window.URL.createObjectURL(this.kontrolki.videoInput.files[0])
+				this.url = window.URL.createObjectURL(this.controls.videoInput.files[0])
 				
 				this.video.src = this.url;
 				
-				if(this.video.duration === NaN){ alert("Coś nie tak. Spróbuj z innym filmem"); return }
+				if(Boolean(this.video.duration)){ alert("Cannot read duration of video. Try loading a different file."); return }
 			}.bind(this);
 		},
-		
+
+		initializeFullScreen: function(){
+						
+			if(!document.fullscreenEnabled
+				&& !document.webkitFullscreenEnabled 
+				&& !document.mozFullScreenEnabled 
+				&& !document.msFullscreenEnabled){
+
+					return;
+			}
+
+			var _fsLookup = {
+				change: ["onfullscreenchange", "onwebkitfullscreenchange", "onmozfullscreenchange", "MSFullscreenChange"],
+				request: ["requestFullscreen", "webkitRequestFullscreen", "mozRequestFullScreen", "msRequestFullscreen"],
+				exit: ["exitFullscreen", "webkitExitFullscreen", "mozCancelFullScreen", "msExitFullscreen"]
+			},
+				_onFSchange = _fsLookup.change.filter( function(el){ return document[el] !== undefined  } )[0];
+
+			this._requestFS = _fsLookup.request.filter( function(el){ return this.videoWrapper[el] !== undefined } )[0];
+			this._exitFS = _fsLookup.exit.filter( function(el){ return document[el] !== undefined } )[0];
+			
+			document[_onFSchange] = this.handleFullScreenChange.bind(this);
+
+			this.controls.fsButton = document.querySelector("#fs");
+		},
+
 		init: function(){
 			
-			this.videoKontener = document.querySelector("#videoKontener");
+			this.videoWrapper = document.querySelector("#videoWrapper");
 			this.video = document.querySelector("#videoBox");
-			this.kontrolki = document.querySelector("#kontrolki");
+			this.controls = document.querySelector("#controls");
 			
 			if(!document.createElement("video").canPlayType) {
-				this.kontrolki.style.display = "none";
+				this.controls.style.display = "none";
 
 				return;
 			};
 			
-			this.kontrolki.playpause = document.querySelector("#playpause");
-			this.kontrolki.stop = document.querySelector("#stop");
-			this.kontrolki.videoProgressRange = document.querySelector("#videoProgressRange");
-			this.kontrolki.timer = document.querySelector("#timer");
-			this.kontrolki.timeProgress = document.querySelector("#timeProgress");
-			this.kontrolki.timeDuration = document.querySelector("#timeDuration");
-			this.kontrolki.eject = document.querySelector("#eject");
-			this.kontrolki.videoInput = document.querySelector("#videoInput");
-			this.kontrolki.mute = document.querySelector("#mute");
-			this.kontrolki.volumeSlider = document.querySelector("#volumeSlider");
-			this.kontrolki.volumeRange = document.querySelector("#volumeRange");
-			this.kontrolki.fsButton = document.querySelector("#fs");
+			this.controls.playpause = document.querySelector("#playpause");
+			this.controls.stop = document.querySelector("#stop");
+			this.controls.videoProgressRange = document.querySelector("#videoProgressRange");
+			this.controls.timer = document.querySelector("#timer");
+			this.controls.timeProgress = document.querySelector("#timeProgress");
+			this.controls.timeDuration = document.querySelector("#timeDuration");
+			this.controls.eject = document.querySelector("#eject");
+			this.controls.videoInput = document.querySelector("#videoInput");
+			this.controls.mute = document.querySelector("#mute");
+			this.controls.volumeSlider = document.querySelector("#volumeSlider");
+			this.controls.volumeRange = document.querySelector("#volumeRange");
 			
-			this.kontrolki.videoProgressRange.value = "0";
+			this.controls.videoProgressRange.value = "0";
 			
-			this.zablokujKontrolki();
-			this.kontrolkiToggler();
-			this.przypiszZdarzenia.init(this);
-			this.zmianaGlosnosci(false);
+			this.initializeFullScreen();
+			this.blockControls();
+			this.controlsToggler();
+			this.assignEvents.init(this);
+			this.volumeChange(false);
 		}
 		
 	}
