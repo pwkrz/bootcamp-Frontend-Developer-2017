68ab5914745eac1e0bf304d8fb24c90476ba04cf
pwkrz <pg.krzeminski@gmail.com>
2018-04-26 21:51:55 +0200

    Added an input field where the user can provide the selector + adjusted .section styling

--- tydzien-2/2.5.-Wrapper-Toggler-dla-element-w-z-drzewa-DOM-master/scripts.js
+++ tydzien-2/2.5.-Wrapper-Toggler-dla-element-w-z-drzewa-DOM-master/scripts.js
@@ -3,9 +3,9 @@ window.onload = function(){
 function Toggler(selector){
 	
 	var selectedElements = document.querySelectorAll(selector);
-	
+
 	this.getElem = function(i){
-		
+	
 		return selectedElements[i]
 		
 	};
@@ -29,32 +29,50 @@ function Toggler(selector){
 		return this
 		
 	}
-	
-	
 }
 
 
-var elem = new Toggler("#section");
-
-elem.prototype = Object.create(Toggler.prototype);
-
-elem.prototype.constructor = elem;
-
+var input = document.querySelector("#input");
 var button = document.querySelector("#button");
-
 var buttonText = button.querySelector("span");
 
-button.addEventListener("click", function() {	
+button.addEventListener("click", function() {
 
-    if(elem.getElem(0).style.display == "none"){
-		buttonText.textContent = "Hide";
-		elem.show();
-    } else {
-		buttonText.textContent = "Show";
-        elem.hide();
-    }
-	
-	this.blur();
+	if( !(document.querySelector(input.value)) ){
+
+		alert("No elements found.");
+
+		return false;
+
+	};
+
+	var flag = true;
+
+	if( !(/h1|section/.test(input.value)) ){
+
+		flag = confirm("Are you sure?");
+
+	};
+
+	if(flag){
+
+		var elem = new Toggler(input.value);
+
+		elem.prototype = Object.create(Toggler.prototype);
+
+		elem.prototype.constructor = elem;
+
+		if(elem.getElem(0).style.display == "none"){
+			buttonText.textContent = "Hide";
+			elem.show();
+		} else {
+			buttonText.textContent = "Show";
+			elem.hide();
+		}
+		
+		this.blur();
+
+	}
 
 }, false);
 
