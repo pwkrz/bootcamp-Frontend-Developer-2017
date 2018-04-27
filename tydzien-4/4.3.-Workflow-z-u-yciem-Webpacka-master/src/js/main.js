a1f4e368fb27d624c40fed107ef0512b893f4750
pwkrz <pg.krzeminski@gmail.com>
2018-04-27 18:47:07 +0200

    Updated dependencies, started fixing bxSlider display

--- tydzien-4/4.3.-Workflow-z-u-yciem-Webpacka-master/src/js/main.js
+++ tydzien-4/4.3.-Workflow-z-u-yciem-Webpacka-master/src/js/main.js
@@ -1,6 +1,11 @@
 import "../sass/main.scss"
 // import {setPosition, stopPropag, hideMenu} from "./menu-events";
-import {init as sliderInit} from "bxslider";
-
+import $ from "jquery";
+import * as bxSlider from "bxslider";
 try { require( "../uploads/"+/\*.*$/ ) }
 catch(err){ console.log( `Funkcja używana do importu obrazków z folderu "Uploads" - ${err}` ) }
+
+$(function(){
+    $('.bxslider').bxSlider();
+  });
+
