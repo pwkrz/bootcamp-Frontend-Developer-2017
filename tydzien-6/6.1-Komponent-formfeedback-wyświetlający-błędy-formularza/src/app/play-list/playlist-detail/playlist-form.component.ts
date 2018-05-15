9d01b1d81ae1d1018c96872c3854ef19d26fc4ab
pwkrz <pg.krzeminski@gmail.com>
2018-05-15 21:45:56 +0200

    Added separate html template for playlist-form

--- tydzien-6/6.1-Komponent-formfeedback-wyświetlający-błędy-formularza/src/app/play-list/playlist-detail/playlist-form.component.ts
+++ tydzien-6/6.1-Komponent-formfeedback-wyświetlający-błędy-formularza/src/app/play-list/playlist-detail/playlist-form.component.ts
@@ -4,36 +4,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'playlist-form',
-  template: `
-    <h5 class="card-title">{{ title }}</h5>
-    <p class="card-text">{{ description }}</p>
-    <form #formRef='ngForm' novalidate="true" (ngSubmit)="save(formRef.valid, formRef.value)">
-      <div class="form-group">
-        <label>Name</label>
-        <input #nameRef='ngModel' [(ngModel)]="playlist.name" required minlength="3" type="text" name="name"
-              [ngClass]='{"is-invalid": (nameRef.touched  || formRef.submitted) && nameRef.errors, "form-control": true}'>
-        <div *ngIf='nameRef.touched || nameRef.dirty || formRef.submitted' class='invalid-feedback'>
-          <small *ngIf='nameRef.errors?.required'>Please provide a name for the playlsit</small>
-          <small *ngIf='nameRef.errors?.minlength'>The name must be at least {{nameRef.errors.minlength.requiredLength}} characters long</small>
-        </div>
-      </div>
-      <div class="form-group">
-        <label>Tracks</label>
-        <input [(ngModel)]="playlist.tracks" type="number" name="tracks" class="form-control">
-      </div>
-      <div class="form-group">
-        <label>Color</label>
-        <input [(ngModel)]="playlist.color" type="color" name="color" class="form-control" style='padding: 0;'>
-      </div>
-      <div class="form-group">
-        <label>Favourite
-        <input [(ngModel)]="playlist.favourite" type="checkbox" name="favourite"></label>
-      </div>
-      <div class="form-group">
-        <button class="btn btn-success">Save</button>
-      </div>
-    </form>
-  `,
+  templateUrl: 'playlist-form.component.html',
   styles: []
 })
 export class PlaylistFormComponent implements OnInit {
@@ -42,6 +13,7 @@ export class PlaylistFormComponent implements OnInit {
   playlist;
   title;
   description;
+  categories = ['yes wave','desindustrialização','désintéressement']
 
   constructor(private activeRoute: ActivatedRoute,
               private playlistService: PlaylistsService,
